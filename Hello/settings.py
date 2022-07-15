2dcb59d7b3dbe2463f68674e86adfca9cd4a6f7d
Utkarsh Shukla <us259990@gmail.com>
2022-07-15 16:35:36 +0530

    h

--- Hello/settings.py
+++ Hello/settings.py
@@ -87,7 +87,29 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'OPTIONS' : {
-            'options': '-c search_path=\"public\"'
+            'options': '-c search_path=\"complaint\"'
+        },
+        'NAME': 'dfb3gc8jfifa5l', 
+        'USER': 'kzabtpjpntwuij', 
+        'PASSWORD': '5930f36915b7d320610693bba16fa1ad8cf2062ad61eea856612011afa76782d',
+        'HOST': 'ec2-44-206-197-71.compute-1.amazonaws.com', 
+        'PORT': '5432',
+    },
+    'comapp': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS' : {
+            'options': '-c search_path=\"comapp\"'
+        },
+        'NAME': 'dfb3gc8jfifa5l', 
+        'USER': 'kzabtpjpntwuij', 
+        'PASSWORD': '5930f36915b7d320610693bba16fa1ad8cf2062ad61eea856612011afa76782d',
+        'HOST': 'ec2-44-206-197-71.compute-1.amazonaws.com', 
+        'PORT': '5432',
+    },
+    'complaint': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS' : {
+            'options': '-c search_path=\"complaint\"'
         },
         'NAME': 'dfb3gc8jfifa5l', 
         'USER': 'kzabtpjpntwuij', 
